1df71d4bb55c5c585d66d44b5ce4fd076f3a95d8
gabriely_paitax <gabrielypaitax@hotmail.com>
2022-11-30 14:19:27 -0300

    Add Wrapper

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,38 @@
 import "../styles/header.css";
 import HeaderLogo from "../assets/imgs/header-logo.svg";
+import Wrapper from "./Wrapper";
 
 function Header() {
   return (
-    <div className="content-header">
-      <img src={HeaderLogo} />
-      <nav className="menu-header">
-        <ul>
-          <li>
-            <a href="#" id="nav-home">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" id="nav-characters">
-              Characters
-            </a>
-          </li>
-          <li>
-            <a href="#" id="nav-comics">
-              Comics
-            </a>
-          </li>
-          <li>
-            <a href="#" id="nav-movies">
-              Movies
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Wrapper>
+      <div className="content-header">
+        <img src={HeaderLogo} />
+        <nav className="menu-header">
+          <ul>
+            <li>
+              <a href="#" id="nav-home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" id="nav-characters">
+                Characters
+              </a>
+            </li>
+            <li>
+              <a href="#" id="nav-comics">
+                Comics
+              </a>
+            </li>
+            <li>
+              <a href="#" id="nav-movies">
+                Movies
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Wrapper>
   );
 }
 
