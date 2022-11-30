a759abc6b1b9ffc5ce5036d58a4ebc96e29c482f
gabriely_paitax <gabrielypaitax@hotmail.com>
2022-11-30 11:30:11 -0300

    Add content home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,32 @@
 import "../styles/home.css";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <div className="content-home">
       <div className="background-img">
         <Header />
+        <section className="informations-home">
+          {/* Texts */}
+          <div className="texts-home">
+            <h1>Marvel developer</h1>
+            <p>
+              The application was built in React and using data provided by
+              <br /> The Marvel Comics API. Here the characters, comics and
+              movies <br />
+              that make up the Marvel library were listed.
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="buttons-home">
+            <a href="#" className="buttons btn-visit-api">VISIT API</a>
+              <a href="#" className="buttons btn-visit-project">VISIT PROJECT</a>
+          </div>
+        </section>
+      </div>
+      <div className="footer-home">
+        <Footer/>
       </div>
     </div>
   );
