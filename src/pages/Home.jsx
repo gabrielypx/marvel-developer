9c8aa1a1800e0820b4dc44ae1159bfc4eaf44a52
gabriely_paitax <gabrielypaitax@hotmail.com>
2022-11-30 14:22:20 -0300

    Add Wrapper; Add button link; Fix footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import "../styles/home.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Wrapper from "../components/Wrapper";
 
 function Home() {
   return (
     <div className="content-home">
       <div className="background-img">
         <Header />
+        <Wrapper>
         <section className="informations-home">
           {/* Texts */}
           <div className="texts-home">
@@ -20,14 +22,14 @@ function Home() {
           </div>
           {/* Buttons */}
           <div className="buttons-home">
-            <a href="#" className="buttons btn-visit-api">VISIT API</a>
-              <a href="#" className="buttons btn-visit-project">VISIT PROJECT</a>
+            <a href="https://developer.marvel.com/"  className="buttons btn-visit-api">VISIT API</a>
+            <a href="https://github.com/gabrielypx/marvel-developer" className="buttons btn-visit-project">VISIT PROJECT</a>
           </div>
         </section>
-      </div>
-      <div className="footer-home">
+        </Wrapper>
         <Footer/>
       </div>
+        
     </div>
   );
 }
